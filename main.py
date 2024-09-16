2c8733e0beadb05854bfe3f7776015bca68a6290
RemiDupont1 <remi.dupont10@gmail.com>
2024-09-16 16:29:00 -0700

    btn

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fasthtml.common import *
+from datetime import datetime
 
-app, rt = fast_app(hdrs=(picolink))
 
+# Set up the app, including daisyui and tailwind for the chat component
+tlink = Script(src="https://cdn.tailwindcss.com"),
+dlink = Link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css")
+app, rt = fast_app(hdrs=(tlink, dlink, picolink), ws_hdr=True)  # Ajout de htmx
 
 @rt("/")
 def get():
@@ -21,6 +25,8 @@ def get():
                         "FastHTML is a new next-generation web framework for fast, scalable web applications with minimal, compact code. It builds on top of popular foundations like ASGI and HTMX. You can now deploy FastHTML with Vercel CLI or by pushing new changes to your git repository.",
                     ),
                 ),
+                Button("Cliquez-moi", hx_post="/hello-world", hx_target="#result", cls="btn btn-warning"),
+                Div(id="result"),  # Moved outside of Group
                 header=(Titled("FastHTML + Vercel + remi")),
                 footer=(
                     P(
@@ -37,5 +43,9 @@ def get():
         ),
     )
 
+@rt("/hello-world", methods=["POST"])
+def hello_world():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return P(f"Hello World! Button clicked at {current_time}")
 
 serve()
